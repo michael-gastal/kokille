a57c68bb357fb144640ee73af00594ff7e768f19
michael gastal <michaelgastal@gmail.com>
2021-05-31 17:08:14 +0200

    create offer model, with validations and dependencies, add dependencies for the user model

---
+++ app/models/offer.rb
@@ -0,0 +1,11 @@
+class Offer < ApplicationRecord
+  has_one :user
+  validates :price_per_day, presence: true, numericality: { only_integer: true }
+  validates :capacity, presence: true, numericality: { only_integer: true }
+  validates :shell_type, presence: true
+  validates :start_available_time, presence: true
+  validates :end_available_time, presence: true
+  validates :location_address, presence: true
+  validates :description, presence: true, length: { minimum: 120 }
+  validates :offer_title, presence: true
+end
