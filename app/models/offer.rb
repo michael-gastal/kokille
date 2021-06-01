a0e21bba703e7ffa26d4c2ef923362d032868c99
Olivier Gil <gil.olivier@live.fr>
2021-06-01 19:56:50 +0400

    Add images for offer

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,7 +1,7 @@
 class Offer < ApplicationRecord
-  belongs_to :user
   has_many_attached :photos
 
+  belongs_to :user
   validates :price_per_day, presence: true, numericality: { only_integer: true }
   validates :capacity, presence: true, numericality: { only_integer: true }
   validates :shell_type, presence: true
