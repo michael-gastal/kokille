a4222dc01e28ce3cde7c2595804f32a44b8bdb49
Olivier Gil <gil.olivier@live.fr>
2021-06-02 16:26:16 +0400

    Add bookmark with pundit

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   devise_for :users
   resources :offers do
-    ressources :bookings, only: [ :new, :create ]
+    resources :bookings, only: [ :new, :create ]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
